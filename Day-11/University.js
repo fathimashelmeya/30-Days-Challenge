191715ddf5af3d3e501e2760838832d13351b4a0
fathimashelmeya <fathimashelmeya@gmail.com>
2026-05-18 18:23:03 +0530

    Create University.js

---
+++ Day-11/University.js
@@ -0,0 +1,37 @@
+let url = "http://universities.hipolabs.com/search?name=";
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async()=>{
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+   let collArr = await getCollage(country);
+
+   Show(collArr);
+});
+
+function Show(collArr){
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for(col of collArr){
+        console.log(col.name);
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+
+}
+
+async function getCollage(country) {
+    try{
+        let res = await axios.get(url+country);
+        return res.data;
+
+    }catch(e){
+        console.log("error:",e);
+        return [];
+
+    }
+    
+}
